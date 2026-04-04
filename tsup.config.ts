6ca0697be1411c8dee6df059c6060a597e0f651d
HeraldSignals <herald@heralddev.io>
2026-04-04 12:09:48 +0300

    add tsup bundle config targeting node20 esm

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["index.ts"],
+  format: ["esm"],
+  target: "node20",
+  outDir: "dist",
+  clean: true,
+  sourcemap: true,
+  dts: false,
+  splitting: false,
+  bundle: true,
+  noExternal: [],
+});
